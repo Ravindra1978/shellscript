5c01c1ba7c5c1b3a3294db8f600ad2f03a05748d
Ravindra1978 <ravindrab44@gmail.com>
2023-11-12 16:36:45 +0530

    creaeted validate function to reduce the lines of code

--- postfix.sh
+++ postfix.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-VALIDATE()
-{
-    if [ $1 -ne 0 ]
-   then
-      echo "Installation is FAILURE..."
-   else
-      echo "Installation is SUCCESS..."
-  fi
-}
+
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Please run the script with root user..."
     exit 1
 fi
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+   then
+      echo "Installing $2 is FAILURE..."
+   else
+      echo "Installing $2 is SUCCESS..."
+  fi
+}
    yum install mailx -y
-   VALIDATE  $?
+   VALIDATE  $? "mailx"
   yum install postfix -y
-  VALIDATE $?
+  VALIDATE $? "postfix"
 
