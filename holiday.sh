31669a6aee2a0b5f93a9ca00d767759e5e596369
Ravindra1978 <ravindrab44@gmail.com>
2023-11-12 11:41:44 +0530

    modified code

--- holiday.sh
+++ holiday.sh
@@ -2,7 +2,7 @@ echo "enter day"
 read today
 if [ $today -eq "sunday" ]
 then 
-   echo "enjoy..$today is holiday"
+   echo "enjoy..$today is sunday and it is holiday today"
    exit 1
 else
    echo "$today is working day...Happy Learning..."
