e978b586f4e583f7de3411c315eb42c200868175
Ravindra1978 <ravindrab44@gmail.com>
2023-11-12 12:32:24 +0530

    validatation check added in code

--- postfix.sh
+++ postfix.sh
@@ -5,3 +5,9 @@ then
     echo "Please run the script with root user..."
 fi
    yum install mailx -y
+   if [ $? -ne 0 ]
+   then
+      echo "maix installation is not installled..."
+   else
+      echo "mailx installed successfully...."
+  fi
