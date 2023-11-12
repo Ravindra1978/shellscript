e3e0387242762a3ef263c7bc52aef409469e7f6a
Ravindra1978 <ravindrab44@gmail.com>
2023-11-12 19:54:02 +0530

    enabled the echo by adding -e

--- postfix.sh
+++ postfix.sh
@@ -17,9 +17,9 @@ VALIDATE()
 {
     if [ $1 -ne 0 ]
    then
-      echo "$R Installing $2 is FAILURE...$N"
+      echo -e "$R Installing $2 is FAILURE...$N"
    else
-      echo "$R Installing $2 is SUCCESS...$N"
+      echo -e "$R Installing $2 is SUCCESS...$N"
   fi
 }
    yum install mailx -y &>>LOGFILE
