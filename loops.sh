e6c08e552d6710ef5c7f94fd04a87618dfea3b4d
Ravindra1978 <ravindrab44@gmail.com>
2023-11-12 20:26:12 +0530

    modified code

--- loops.sh
+++ loops.sh
@@ -2,9 +2,9 @@ for i in {1..10}
 do
     echo $i
 done
- name=["ravi" "suma" "Radha"]
- echo ${name[0]}
+ name=("ravi" "suma" "Radha")
+ echo "${name[0]}"
  for i in $@
 do
-  echo $i
+  echo "$i"
 done 
