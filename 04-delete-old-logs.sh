224adad401702719240c76cb448cbc45716c502c
Ravindra1978 <ravindrab44@gmail.com>
2023-11-13 16:37:15 +0530

    delete the old logs which are greater than two weeks ago

---
+++ 04-delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This program delete the files which are 2 weeks ago.
+APP_LOGS_DIR=/home/ec2-user/app-logs
+    LOGS_DIR=/home/ec2-user/shellscript-logs
+    DATE=$(date -u)
+    SCRIPT_NAME=$0
+    LOGFILE=$LOGS_DIR/$0-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR  -name "*.log" -type f -mtime +14)
+echo "Files to delete are $FILES_TO_DELETE"
+
+while read line
+do 
+   echo "Deleting $line" &>> $LOGFILE
+   rm -rf $line
+done <<< $FILES_TO_DELETE
