c0f03e8a848565cac781ccc7e0ac3eb048793371
Ravindra1978 <ravindrab44@gmail.com>
2023-11-14 20:12:07 +0530

    disk usage check using shell script

---
+++ disk_usage.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#color &validatations
+#log directions
+DATE=$(date -u)
+SCRIPT_NAME=$0
+LOGFILE_DIR=/tmp
+LOGFILE=$LOGFILE_DIR/$SCRIPT_NAME-$DATE.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+VALIDATIION()
+{
+    if [ $1 -ne 0 ]
+    then
+        echo "Instalation of $2 is $R FAILURE.....$N"
+    else
+        echo "Instalatoion of $2 is $G SUCCESS...$N"
+    fi
+}
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+DISK_USAGE_THRESHOLD=1
+#IFS--Internal Field Separator
+while IFS= read line
+do
+   echo "output: $line"
+done <<< $DISK_USAGE
+
