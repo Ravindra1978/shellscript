eaf55be931fc30e6864fab2ffa62dd2fda3a3334
Ravindra1978 <ravindrab44@gmail.com>
2023-11-24 12:45:57 +0530

    error fixed

--- 05_cmdec2.sh
+++ 05_cmdec2.sh
@@ -7,9 +7,9 @@ for i in ${NAMES[@]}
 do
  if [[ $i == "mongodb" || $i == "mysql" ]]
  then
-    INSTANCE_TYPE ="t3.medium"
+    INSTANCE_TYPE="t3.medium"
 else
-    INSTANCE_TYPE ="t2.micro"
+    INSTANCE_TYPE="t2.micro"
 fi
     echo "creating $i instance"
     #aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --query 'Instances[0].PrivateIpAddress'
