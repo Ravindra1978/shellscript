a9acf93b2010ab0e172e7da057624efaa9a51a69
Ravindra1978 <ravindrab44@gmail.com>
2023-11-24 12:34:03 +0530

    creating Roboshop ec2 machines using aws CLI

--- 05_cmdec2.sh
+++ 05_cmdec2.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "cart" "user" "shipping" "payments" "dispatch" "web")
+INSTANCE_TYPE=""
+IMAGE-ID=ami-03265a0778a880afb
+SECURITY_GROUP_ID=sg-02aff670556d3d4d0
 for i in ${NAMES[@]}
 do
-  echo "NAME: $i"
+ if [[ $i == "mongodb" || $i == "mysql" ]]
+ then
+    INSTANCE_TYPE = "t3.medium"
+else
+    INSTANCE_TYPE = "t2.micro"
+fi
+    echo "creating $i instance"
+    #aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --query 'Instances[0].PrivateIpAddress'
+    #--tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]'
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE  --security-group-ids $SECURITY_GROUP_ID --query 'Instances[0].PrivateIpAddress' --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$i}]'
 done
   
